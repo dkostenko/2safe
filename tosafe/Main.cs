63abbb73686b2a9818e135771f36c11c27c0f613
Pavel Gurov <pashathebeast@MacBook-Air.local>
2012-11-14 01:15:34 +0400

    tester console interface

--- tosafe/Main.cs
+++ tosafe/Main.cs
@@ -7,16 +7,81 @@ namespace tosafe
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!!");
-			string cmd = "chk_mail";
-			string data = "&email=pashaisthebest@hotmail.com";
 
-			string respond = Connection.sendRequest("GET", cmd, data);
-			Console.WriteLine(respond);
+			ConsoleInterface ci = new ConsoleInterface();
+			ci.Run();
+
+//			string cmd = "chk_mail";
+//			string data = "&email=pashaisthebest@hotmail.com";
+//
+//			string respond = Connection.sendRequest("GET", cmd, data);
+//			Console.WriteLine(respond);
+//
+//
+//			Console.WriteLine("Для завершения программы нажмите ENTER");
+//			Console.ReadLine();
+		}
+	}
+
+	class ConsoleInterface
+	{
+		string mainMenu = "\nMAIN MENU\n\n1.     Проверить доступность email\n2.     Проверить доступность login\n3.     Авторизоваться\nESC.   Выход\n\nНажмите соответствующую клавишу";
+		string emailMessage = "\nВведите email который хотите проверить на доступность: ";
+		string loginMessage = "\nВведите login который хотите проверить на доступность: ";
+		string loginPrompt = "\nВведите логин :";
+		string passPrompt = "\nВведите пароль :";
+		string cmd, data, respond;
+
+		bool flag = true;
 
 
-			Console.WriteLine("Для завершения программы нажмите ENTER");
-			Console.ReadLine();
+		public void Run ()
+		{
+
+
+			while (flag) 
+			{
+				Console.Clear ();
+				Console.Write (mainMenu);
+				ConsoleKeyInfo key = Console.ReadKey();
+
+				switch (key.Key)
+				{
+				case (ConsoleKey.Escape):	
+					flag = false;
+					break;
+				case (ConsoleKey.D1):
+					Console.Clear();
+					Console.Write(emailMessage);
+					cmd = "chk_mail";
+					data = "&email=" + Console.ReadLine();
+					respond = Connection.sendRequest("GET", cmd, data);
+					Console.WriteLine(respond);
+					Console.ReadLine();
+					break;
+				case (ConsoleKey.D2):
+					Console.Clear();
+					Console.Write(loginMessage);
+					cmd = "chk_login";
+					data = "&login=" + Console.ReadLine();
+					respond = Connection.sendRequest("GET", cmd, data);
+					Console.WriteLine(respond);
+					Console.ReadLine();
+					break;
+				case (ConsoleKey.D3):
+					Console.Clear();
+					Console.Write(loginPrompt);
+					cmd = "auth";
+					data = "&login=" + Console.ReadLine();
+					Console.Write(passPrompt);
+					data += "&password=" + Console.ReadLine();
+					respond = Connection.sendRequest("GET", cmd, data);
+					Console.WriteLine(respond);
+					Console.ReadLine();
+					break;
+				}
+			}
+
 		}
 	}
 }
