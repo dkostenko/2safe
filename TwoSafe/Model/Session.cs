4ec9d20d264a73876af7960b367e108bfaf1e1ce
dkostenko <kostenko.personal@gmail.com>
2012-12-10 00:14:16 +0400

    class session fixed

--- TwoSafe/Model/Session.cs
+++ TwoSafe/Model/Session.cs
@@ -3,52 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwoSafe.Model
 {
-    static class Session
+    public static class Session
     {
-        //поля
-        private static string email, lang, account, token, mainFolderPath;
+        private static string lang, token;
 
         /// <summary>
-        /// Начинает сессию
+        /// Начинает сессию. Если возвращает False - необходимо авторизироваться заново. True - сессия началась удачно.
         /// </summary>
-        public static void startSession(string email, string lang, string account, string token, string mainFolderPath)
+        public static bool startSession()
         {
-            Email = email;
-            Lang = lang;
-            Account = account;
+            try
+            {
+                StreamReader sr = new StreamReader("Cookie.txt");
+                string textFromFile = sr.ReadToEnd();
+                sr.Close();
+                char[] separators = new char[] { '\n' };
+                string[] cookie = textFromFile.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                if (cookie.Length == 0) 
+                { 
+                    return false; 
+                }
+
+                lang = cookie[1];
+                Dictionary<string, dynamic> response = Controller.ApiTwoSafe.getPersonalData(cookie[0]);
+
+                if (response.ContainsKey(response["error_code"]))
+                {
+                    return false;
+                }
+
+                token = cookie[0];
+                lang = response["response"]["personal"]["lang"];
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }    
+        }
+
+        /// <summary>
+        /// Если не получилось начать сессию без авторизации, то необходимо "обновить" сессию валидным токеном?
+        /// </summary>
+        public static void refreshSession(string token)
+        {
+            if (Lang == null)
+            {
+                Lang = "ru";
+            }
             Token = token;
-            MainFolderPath = mainFolderPath;
+            saveSession();
         }
 
-
-        //свойства
-        public static string Email
+        /// <summary>
+        /// Сохраняет сессию в локальный файл. Записывает только токен и текущий язык программы
+        /// </summary>
+        public static void saveSession()
         {
-            get { return email; }
-            set { email = value; }
+            StreamWriter sw = new StreamWriter("Cookie.txt", false);
+            sw.WriteLine(Token);
+            sw.WriteLine(Lang);
+            sw.Close();
         }
+
+
+        /// <summary>
+        /// Текущий язык программы
+        /// </summary>
         public static string Lang
         {
             get { return lang; }
             set { lang = value; }
         }
-        public static string Account
-        {
-            get { return account; }
-            set { account = value; }
-        }
+
+        /// <summary>
+        /// Валидный токен
+        /// </summary>
         public static string Token
         {
             get { return token; }
             set { token = value; }
-        }
-        public static string MainFolderPath
-        {
-            get { return mainFolderPath; }
-            set { mainFolderPath = value; }
         }
     }
 }
