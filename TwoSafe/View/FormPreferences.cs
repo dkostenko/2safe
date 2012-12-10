63155cb1a8d990f80e93f70f71c0736a09b16269
dkostenko <kostenko.personal@gmail.com>
2012-12-10 15:05:03 +0400

    comments in API class added

--- TwoSafe/View/FormPreferences.cs
+++ TwoSafe/View/FormPreferences.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Collections.Specialized;
 using System.Globalization;
 using System.Threading;
 using System.Resources;
